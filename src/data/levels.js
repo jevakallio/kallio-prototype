ea72dd2232fde81c1e60ed311ec0cfa07d0bd7de
Jani Eväkallio <jani.evakallio@gmail.com>
2015-12-06 16:19:42 +0000

    Update level data

--- src/data/levels.js
+++ src/data/levels.js
@@ -1,25 +1,43 @@
 'use strict';
 
+function encode(answer) {
+  return btoa(answer);
+}
+
 module.exports = [
   {
     levelIndex: 0,
-    levelExplanation: 'Explanation text for the answer...',
-    levelClue: 'Find the years this bridge was built, and do the maths.',
-    levelAnswer: '-2',
+    levelExplanation: '',
+    levelClue: `Let's start easy. Find the building years of the bridge and do the maths.`,
+    levelAnswer: encode('-2'),
     levelAnswerIsExact: true
   },
   {
     levelIndex: 1,
-    levelExplanation: 'Phew, at least I’m still in London. But instead of a phone, all I found in my pocket is this coat check ticket. I don’t know where it’s from though, but I guess there’s nothing to it but to be brave and get walking. Fingers crossed it’s nearby.',
-    levelClue: 'Bury the holy hatchet.  What comes first above wine?',
-    levelAnswer: 'quality',
+    levelExplanation: '',
+    levelClue:`Find an even longer bridge, towards brutal grey monsters. On the way, you'll find a gift from the east. Who is it from?`,
+    levelAnswer: encode('Moskova, Moskva, Mokba, Moscow'),
     levelAnswerIsExact: false
   },
   {
     levelIndex: 2,
-    levelExplanation: "They seemed surprised to see me, but I got my jacket. There were two things in the pocket: my phone and this piece of paper. Glad to have my phone back, it'll make it a lot easier to jump around town!",
-    levelClue: "Once in the chair, what's framed on the right?",
-    levelAnswer: 'razor',
+    levelExplanation: '',
+    levelClue: `Turn away from this communist monument and head towards a monument to capitalism. Once there, head into the bazaar to spy a knight in shining armor. What swims near him?`,
+    levelAnswer: encode('fish, pike, perch, hauki, kala'),
     levelAnswerIsExact: false
-  }
+  },
+  {
+    levelIndex: 3,
+    levelExplanation: '',
+    levelClue: `From a protected fighter, pass unprotected fighters to find a man on the wall. Who was born here?`,
+    levelAnswer: encode('waltari'),
+    levelAnswerIsExact: false
+  },
+  {
+    levelIndex: 4,
+    levelExplanation: '',
+    levelClue: `Bend around the copper tower, and find two women in the distance. If ASU is EKI, what is EITA?`,
+    levelAnswer: encode('ring'),
+    levelAnswerIsExact: true
+  },
 ];
